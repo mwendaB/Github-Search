853772d3baf1989a72d3a72c79412537886e33aa
mwendaB <brian.mwenda@moringaschool.com>
2021-10-17 21:25:56 +0300

    polishing coded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,28 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgModule } from '@angular/core';
+import { HttpClient,HttpHeaders }from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
 import { ProfileComponent } from './profile/profile.component';
-
+import { ProfileService } from './profile.service'
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule }from '@angular/forms';
+import { DateCountPipe } from './date-count.pipe';
+import { NavbarComponent } from './navbar/navbar.component';
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
-    ProfileComponent
+    ProfileComponent,
+    DateCountPipe,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [ProfileService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
