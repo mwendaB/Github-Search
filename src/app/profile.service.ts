29651a34745e32dae227286f4f0eace5eaa327a9
mwendaB <brian.mwenda@moringaschool.com>
2021-10-17 14:54:00 +0300

    add the service ts file

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -3,3 +3,13 @@ import { HttpClient }from '@angular/common/http';
 @Injectable({
     providedIn: 'root'
   })
+
+  export class ProfileService {
+
+    private username:string;
+    private client_id = "0ddd798ddad906269c18";
+    private client_secret = "8d00ece48247a8eaffc420181de3c83776e0310c";
+    constructor(private http:HttpClient) { 
+        console.log("Service is now ready!");
+        this.username = "mwendaB";
+      }
